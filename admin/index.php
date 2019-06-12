63ebf107dd941eec35f418d5d328824758b95ecd
ThuyDuong2411 <49835907+ThuyDuong2411@users.noreply.github.com>
2019-06-12 15:14:06 +0700

    Update exercise

---
+++ admin/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Start your development with a Dashboard for Bootstrap 4.">
+    <meta name="author" content="Creative Tim">
+    <title>Admin</title>
+    <!-- Favicon -->
+    <link href="./assets/img/brand/favicon.png" rel="icon" type="image/png">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+    <!-- Icons -->
+    <link href="./assets/vendor/nucleo/css/nucleo.css" rel="stylesheet">
+    <link href="./assets/vendor/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet">
+    <!-- Argon CSS -->
+    <link type="text/css" href="./assets/css/argon.css?v=1.0.0" rel="stylesheet">
+</head>
+
+<body>
+<?php include('includes/sidenav.php') ?>
+<!-- Main content -->
+<div class="main-content">
+    <?php include('includes/navbar.php') ?>
+    <!-- Header -->
+    <?php include('includes/header.php') ?>
+    <!-- Page content -->
+
+<!-- Argon Scripts -->
+<!-- Core -->
+<script src="./assets/vendor/jquery/dist/jquery.min.js"></script>
+<script src="./assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<!-- Optional JS -->
+<script src="./assets/vendor/chart.js/dist/Chart.min.js"></script>
+<script src="./assets/vendor/chart.js/dist/Chart.extension.js"></script>
+<!-- Argon JS -->
+<script src="./assets/js/argon.js?v=1.0.0"></script>
+</body>
+</html>
